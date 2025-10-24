fa5f97964b73915c166c7a471ea79672db98385c
JensBaekelandt <baekelandtjensmail@gmail.com>
2025-10-24 15:46:41 +0200

    set code on a behind file

---
+++ BlazorWebAppMovies/Components/Pages/MoviePages/Details.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazorWebAppMovies.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWebAppMovies.Components.Pages.MoviePages
+{
+    public partial class Details
+    {
+        private Movie? movie;
+
+        [SupplyParameterFromQuery]
+        private int Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            using var context = DbFactory.CreateDbContext();
+            movie = await context.Movie.FirstOrDefaultAsync(m => m.Id == Id);
+
+            if (movie is null)
+            {
+                NavigationManager.NavigateTo("notfound");
+            }
+        }
+    }
+}
